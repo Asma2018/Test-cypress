ed02bff73f46590a387fe3e3897e97de8419f4cf
Asma2018 <rababhina@hotmail.com>
2020-09-22 20:09:52 +0200

    login test

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -7,5 +7,26 @@ describe("My first test", () => {
 describe("My first test", () => {
   it("Trello home page", () => {
     cy.visit("https://trello.com/login");
+    cy.get("form-login");
+  });
+  it("should fill login form and redirect to homepage", () => {
+    // Fill the username
+    cy.get('input[name="user"]')
+      .type("rababhina@hotmail.com")
+      .should("have.value", "rababhina@hotmail.com");
+
+    // Fill the password
+    cy.get('input[name="password"]')
+      .type("123$567")
+      .should("have.value", "123$567");
+
+    // Locate and submit the form
+    cy.get("login").submit();
+
+    // Verify the app redirected you to the homepage
+    cy.location("pathname", { timeout: 10000 }).should("eq", "/");
+
+    // Verify the page title is "Home"
+    cy.title().should("eq", "Home");
   });
 });
